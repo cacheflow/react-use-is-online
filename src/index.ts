535af1f69a043ed18fe6f74100ffcd2089f24e4b
Lex Alexander <l.alexander10@gmail.com>
2020-03-03 17:43:15 -0800

    feat(Navigator): Added additional check for navigator

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
 import { useEffect, useState } from 'react';
 
 export interface IValues {
-  error: null | string
-  isOffline: boolean
-  isOnline: boolean
-
+  error: null | string;
+  isOffline: boolean;
+  isOnline: boolean;
 }
 
-const NOT_BROWSER_ENV_ERROR = 'useIsOnline only works in a browser environment.';
+const NOT_BROWSER_ENV_ERROR =
+  'useIsOnline only works in a browser environment.';
 
-const notABrowserEnv = typeof window === 'undefined'
+const notABrowserEnv = typeof window === 'undefined';
+
+const navigatorNotPresent = typeof navigator === 'undefined';
 
 const useIsOnline = (): IValues => {
-
-  if (notABrowserEnv) {
+  if (notABrowserEnv || navigatorNotPresent) {
     return {
       error: NOT_BROWSER_ENV_ERROR,
       isOnline: false,
-      isOffline: false
-    }
+      isOffline: false,
+    };
   }
 
   const [isOnline, setOnlineStatus] = useState(window.navigator.onLine);
-
 
   useEffect(() => {
     const toggleOnlineStatus = () => setOnlineStatus(window.navigator.onLine);
@@ -33,14 +33,14 @@ const useIsOnline = (): IValues => {
     return () => {
       window.removeEventListener('online', toggleOnlineStatus);
       window.removeEventListener('offline', toggleOnlineStatus);
-    }
-  }, [isOnline])
+    };
+  }, [isOnline]);
 
   return {
     error: null,
     isOffline: !isOnline,
     isOnline,
-  }
-}
+  };
+};
 
-export { useIsOnline }
+export { useIsOnline };
